f97a7fa0edf144c5c5a4ff5dd701fc08e02221c9
Straytora <55440714+Straytora@users.noreply.github.com>
2020-02-17 13:08:09 +0100

    Add files via upload

---
+++ Rook.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleChess
+{
+    internal class Rook : Piece
+    {
+        public Rook()
+        {
+            Symbol = "TRN";
+        }
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
